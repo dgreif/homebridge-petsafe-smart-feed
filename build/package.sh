8904dab16c154895ac10727bd0f1059a9d054e0a
dgreif <dustin.greif@gmail.com>
2020-01-23 17:01:16 -0700

    fix: include auth-cli in release

--- build/package.sh
+++ build/package.sh
@@ -7,6 +7,7 @@ mkdir -p release/lib
 cp -R lib/* release/lib
 cp package.json release
 cp package-lock.json release
+cp petsafe-auth-cli.js release
 cp LICENSE release
 cp README.md release
 cp CHANGELOG.md release
